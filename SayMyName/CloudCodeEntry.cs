82fbfdd8c0584b3d8807aa52c7c4f380351de416
powercore2000 <bcory00@yahoo.com>
2023-03-28 10:15:23 -0700

    Inital Code Commit

---
+++ SayMyName/CloudCodeEntry.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Services.CloudCode.Core;
+
+namespace SayMyName
+{
+    public class CloudCodeEntry
+    {
+        [CloudCodeFunction("SayHello")]
+        public string Hello(string name)
+        {
+            return $"Hello, my is {name}!";
+        }
+
+    }
+}
